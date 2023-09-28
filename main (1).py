b6f54901d96aae45e28c779c3db0147233464ad0
asmku259c2s22900 <144468524+asmku259c2s22900@users.noreply.github.com>
2023-09-28 19:51:25 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,24 +1,19 @@
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
-def sort_students(student_list):
-    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
-    return sorted_students
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-# Test the function with a list of student objects
-students = [
-    Student("Alice", "101", 3.8),
-    Student("Bob", "102", 3.5),
-    Student("Charlie", "103", 3.9),
-    Student("David", "104", 3.7),
-    Student("Eve", "105", 3.6),
-]
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
 
-sorted_students = sort_students(students)
+# Creating objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
 
-# Print the sorted list of students
-for student in sorted_students:
-    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+# Calling the play() method for each object
+batsman.play()
+bowler.play()
